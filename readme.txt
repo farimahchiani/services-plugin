f1dcbc868de8cf564eef10bd4222bd67822f7318
Farimah Chiani <chiani.farimah@yahoo.com>
2026-06-30 17:26:44 +0330

    Initial plugin bootstrap

---
+++ readme.txt
@@ -0,0 +1,15 @@
+=== Services Plugin ===
+
+Contributors: Farimah Chiani
+
+Requires at least: 6.6
+
+Tested up to: 6.8
+
+Requires PHP: 8.1
+
+Stable tag: 1.0.0
+
+License: GPLv2 or later
+
+Simple plugin for managing services and displaying them in a Gutenberg carousel.
---
+++ services-plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Services Plugin
+ * Plugin URI: https://github.com/farimahchiani/services-plugin
+ * Description: Custom Services plugin with Gutenberg carousel block.
+ * Version: 1.0.0
+ * Requires at least: 6.6
+ * Requires PHP: 8.1
+ * Author: Farimah Chiani
+ * License: GPL v2 or later
+ * Text Domain: services-plugin
+ * Domain Path: /languages
+ */
+
+defined( 'ABSPATH' ) || exit;
