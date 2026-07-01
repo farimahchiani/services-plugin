f7fe2c483bb94de4ed62d09f9d4dcad5ed291879
Farimah Chiani <chiani.farimah@yahoo.com>
2026-07-01 13:08:36 +0330

    Add block inspector controls

--- blocks/services-carousel/index.asset.php
+++ blocks/services-carousel/index.asset.php
@@ -1,5 +1,11 @@
 <?php
 return array(
-    'dependencies' => array('wp-blocks', 'wp-element', 'wp-i18n'),
-    'version'      => '1.0.0',
+    'dependencies' => array(
+        'wp-blocks',
+        'wp-element',
+        'wp-i18n',
+        'wp-block-editor',
+        'wp-components',
+    ),
+    'version' => '1.0.0',
 );
--- blocks/services-carousel/index.js
+++ blocks/services-carousel/index.js
@@ -1,19 +1,127 @@
-const { registerBlockType } = wp.blocks;
 
-registerBlockType('services/carousel', {
-    title: 'Services Carousel',
-    icon: 'images-alt2',
-    category: 'widgets',
+(function (blocks, element, blockEditor, components) {
 
-    edit() {
-        return wp.element.createElement(
-            'p',
-            null,
-            'Services Carousel Block'
-        );
-    },
+    var el = element.createElement;
 
-    save() {
-        return null;
-    }
-});
+    var InspectorControls = blockEditor.InspectorControls;
+
+    var PanelBody = components.PanelBody;
+
+    var RangeControl = components.RangeControl;
+
+    var SelectControl = components.SelectControl;
+
+    blocks.registerBlockType('services/carousel', {
+
+        edit: function (props) {
+
+            return [
+
+                el(
+                    InspectorControls,
+                    {},
+
+                    el(
+                        PanelBody,
+                        {
+                            title: 'Query Settings',
+                            initialOpen: true
+                        },
+
+                        el(RangeControl, {
+
+                            label: 'Number of Services',
+
+                            value: props.attributes.number,
+
+                            min: 1,
+
+                            max: 20,
+
+                            onChange: function (value) {
+
+                                props.setAttributes({
+
+                                    number: value
+
+                                });
+
+                            }
+
+                        }),
+
+                        el(SelectControl, {
+
+                            label: 'Display Order',
+
+                            value: props.attributes.order,
+
+                            options: [
+
+                                {
+
+                                    label: 'Newest',
+
+                                    value: 'DESC'
+
+                                },
+
+                                {
+
+                                    label: 'Oldest',
+
+                                    value: 'ASC'
+
+                                }
+
+                            ],
+
+                            onChange: function (value) {
+
+                                props.setAttributes({
+
+                                    order: value
+
+                                });
+
+                            }
+
+                        })
+
+                    )
+
+                ),
+
+                el(
+
+                    'p',
+
+                    {},
+
+                    'Services Carousel'
+
+                )
+
+            ];
+
+        },
+
+        save: function () {
+
+            return null;
+
+        }
+
+    });
+
+})(
+
+    window.wp.blocks,
+
+    window.wp.element,
+
+    window.wp.blockEditor,
+
+    window.wp.components
+
+);
